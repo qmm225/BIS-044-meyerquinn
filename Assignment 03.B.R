4bdcd9361b34b86605b8bed1065592af7252f656
qmm225 <qmm225@lehigh.edu>
2021-10-06 22:20:57 -0400

    Assignment 03B

---
+++ Assignment 03.B.R
@@ -0,0 +1,21 @@
+install.packages("readr")
+
+library(readr)
+PresidentDF <- read_csv("/Users/janemeyer/Downloads/BIS-044-meyerquinn/data/PRESIDENT_precinct_general.csv")
+View(PresidentDF)
+
+Biden <- 0
+Trump <- 0
+
+for (i in 1:length(PresidentDF$party_simplified)){
+  votes <- PresidentDF$party_simplified[i]
+  if (!is.na(votes)){
+    if(PresidentDF$party_simplified[i] == "DEMOCRAT") {
+      Biden = Biden + PresidentDF$votes[i]}
+    if(PresidentDF$party_simplified[i] == "REPUBLICAN"){
+      Trump = Trump + PresidentDF$votes[i]}}}
+
+
+cat("Trump:", Trump)
+cat("Biden:", Biden)
+
